69edc834e25aa40cfb8f0376803faedca944449c
Mark Nuiver <marknuiver@hoppinger.com>
2018-07-12 00:06:43 +0200

    Prettier formating

--- src/App.ts
+++ src/App.ts
@@ -1,25 +1,25 @@
-import * as bodyParser from 'body-parser';
-import * as express from 'express';
-import * as logger from 'morgan';
-import * as path from 'path';
+import * as bodyParser from 'body-parser'
+import * as express from 'express'
+import * as logger from 'morgan'
+import * as path from 'path'
 
 // Creates and configures an ExpressJS web server.
 class App {
   // ref to Express instance
-  public express: express.Application;
+  public express: express.Application
 
   // Run configuration methods on the Express instance.
   constructor() {
-    this.express = express();
-    this.middleware();
-    this.routes();
+    this.express = express()
+    this.middleware()
+    this.routes()
   }
 
   // Configure Express middleware.
   private middleware(): void {
-    this.express.use(logger('dev'));
-    this.express.use(bodyParser.json());
-    this.express.use(bodyParser.urlencoded({ extended: false }));
+    this.express.use(logger('dev'))
+    this.express.use(bodyParser.json())
+    this.express.use(bodyParser.urlencoded({ extended: false }))
   }
 
   // Configure API endpoints.
@@ -27,15 +27,15 @@ class App {
     /* This is just to get up and running, and to make sure what we've got is
      * working so far. This function will change when we start to add more
      * API endpoints */
-    const router = express.Router();
+    const router = express.Router()
     // placeholder route handler
     router.get('/', (req, res, next) => {
       res.json({
         message: 'Hello World!'
-      });
-    });
-    this.express.use('/', router);
+      })
+    })
+    this.express.use('/', router)
   }
 }
 
-export default new App().express;
+export default new App().express
