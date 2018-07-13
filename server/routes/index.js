d8015e38be4c8dd1b6b8cc8950a0737e701975f7
Mark Nuiver <marknuiver@hoppinger.com>
2018-07-13 17:30:18 +0200

    Add full crud functionality for todos

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,4 +1,5 @@
 const todosController = require('../controllers').todos
+const todoItemsController = require('../controllers').todoItems
 
 module.exports = app => {
   app.get('/api', (req, res) =>
@@ -9,4 +10,8 @@ module.exports = app => {
 
   app.post('/api/todos', todosController.create)
   app.get('/api/todos', todosController.list)
+  app.post('/api/todos/:todoId/items', todoItemsController.create)
+  app.get('/api/todos/:todoId', todosController.retrieve)
+  app.put('/api/todos/:todoId', todosController.update);
+  app.delete('/api/todos/:todoId', todosController.destroy)
 }
