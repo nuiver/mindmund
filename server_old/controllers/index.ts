394cfd0bafbdf9df97a0698a2d0f058a961cf4fc
Mark Nuiver <marknuiver@hoppinger.com>
2018-08-02 14:26:07 +0200

    Remove old ts version

--- server_old/controllers/index.ts
+++
@@ -1,9 +0,0 @@
-// import todos from './todos'
-// import todoItems from './todoitems'
-// // const todos = require('./todos')
-// // const todoItems = require('./todoitems')
-
-// export = {
-//   todos,
-//   todoItems,
-// }
