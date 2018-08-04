b38a6e290b228969c61bcfcdfcb12751a2f056c4
Mark Nuiver <marknuiver@hoppinger.com>
2018-08-04 13:22:48 +0200

    Retrieve single todo

--- src/server/controllers/todos/todo.get.ts
+++ src/server/controllers/todos/todo.get.ts
@@ -8,3 +8,36 @@ export function list(req: Request, res: Response) {
     .then(todos => res.status(200).send(todos))
     .catch(error => res.status(400).send(error))
 }
+
+export function retrieve(req: Request, res: Response) {
+  return TodosDao
+    .findById(req.params.todoId)
+    .then(todo => {
+          if (!todo) {
+            return res.status(404).send({
+              message: 'Todo Not Found',
+            });
+          }
+          return res.status(200).send(todo);
+        })
+    .catch(error => res.status(400).send(error))
+}
+
+// retrieve(req, res) {
+//   return Todo
+//     .findById(req.params.todoId, {
+//       include: [{
+//         model: TodoItem,
+//         as: 'todoItems',
+//       }],
+//     })
+//     .then(todo => {
+//       if (!todo) {
+//         return res.status(404).send({
+//           message: 'Todo Not Found',
+//         });
+//       }
+//       return res.status(200).send(todo);
+//     })
+//     .catch(error => res.status(400).send(error));
+// }
